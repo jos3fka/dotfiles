0ca217b35096edac844bedc14c3abb0ed2370797
Jos3fka <jos3fka@gmail.com>
2023-05-13 01:30:20 +0200

    rearranged workspaces

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -2,6 +2,12 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "javascript", "python", "latex" },
 
+  -- Workaround for broken python file indentation
+  indent = {
+      enable = false,
+      disable = { "python" }
+  },
+
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
